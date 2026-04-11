18b2179fee34d6246f41453282d1f693efd5fe50
Getulio Souza <getuliosouza8@gmail.com>
2026-04-11 18:24:18 -0300

    params

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express');
 
 const server = express();
 
-server.get('/hello', (req, res)=> {
- return res.json({
-    title: 'hello world',
-    message: 'hello my friend, how are you?'
+server.get('/hello', (req, res) => {
+    //obtendo os paramentos para a rota
+    const { nome, idade } = req.query;
+
+    return res.json({
+        title: 'hello world',
+        message: `hello, ${nome}, how are you?`,
+        idade: idade
+    })
 })
+
+//nome
+
+server.get('/hello:nome', (req, res) => {
+    const nome = req.params.nome;
+
+    return res.json({
+        title: 'hello world 1',
+        message: `hello, ${nome}, how are you?`,
+    })
 })
 
 server.listen(3000)
+
+// https://www.youtube.com/watch?v=5UgIxA32Q5I
