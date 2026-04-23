0576beee96376864acbb69f621ca0a20293700f4
getulio.souza <getulio.souza@evolutix.com.br>
2026-04-23 20:19:48 -0300

    feature: database-and-customer-js

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 module.exports = {
   dialect: "postgres",
   host: "localhost",
+  port: 5432,
   username: "postgres",
-  password: "secret",
+  password: "123456",
   database: "teste-dominando-nodejs",
   define: {
     timestamp: true, //cria duas colunas - createdAt, updatedAt,
