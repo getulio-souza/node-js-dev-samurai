1c1ac86a995813cab191b28b30786c3c925babde
getulio.souza <getulio.souza@evolutix.com.br>
2026-05-10 20:10:51 -0300

    parametros-de-consulta

--- src/app/controllers/customersController.js
+++ src/app/controllers/customersController.js
@@ -1,4 +1,6 @@
+import { Op } from "sequelize";
 import Customer from "../models/customer"
+import {parseISO} from "date-fns"
 
 //a ideia agora nao eh mais retornar o array de customers, mas sim os registros do nosso banco de dados
 
@@ -17,7 +19,7 @@ class CustomerController {
 
     //parametros de consulta
     const {
-      name, 
+      name,
       email,
       status,
       createdBefore,
@@ -25,7 +27,80 @@ class CustomerController {
       updatedBefore,
       updatedAfter,
       sort
-    } = req.query
+    } = req.query;
+
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 25;
+
+    let where = {};
+    let order = [];
+
+    if (name) {
+      where = {
+        ...where,
+        name: {
+          [Op.iLike]: name,
+        }
+      }
+    }
+
+    if (email) {
+      where = {
+        ...where,
+        email: {
+        [Op.iLike]: email
+        }
+      }
+    }
+
+    if (status) {
+      where = {
+        ...where,
+        status: {
+          [Op.in]: status.split(",").map(item => item.toUpperCase());
+        }
+      }
+    }
+
+     if (createdBefore) {
+      where = {
+        ...where,
+        createdBefore: {
+          [Op.gte]: parseISO(createdBefore)
+        }
+      }
+    }
+
+    if (createdAfter) {
+      where = {
+        ...where,
+        createdAfter: {
+          [Op.lte]: parseISO(createdAfter)
+        }
+      }
+    }
+
+    if (updatedBefore) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.gte]: parseISO(updatedBefore)
+        }
+      }
+    }
+
+    if (updatedAfter) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.lte]: parseISO(updatedAfter)
+        }
+      }
+    }
+
+    if (sort) {
+     order = sort.split(",").map(item => item.split(":"))
+    }
 
 
     const data = await Customer.findAll({
