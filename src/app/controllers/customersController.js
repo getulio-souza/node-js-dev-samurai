9bfda4aa15c8a23f6d1cb39ce74b409e5c401d92
getulio.souza <getulio.souza@evolutix.com.br>
2026-05-18 20:57:48 -0300

    updating-methods

--- src/app/controllers/customersController.js
+++ src/app/controllers/customersController.js
@@ -160,43 +160,101 @@ class CustomerController {
 
   //o metodo create recebe todos os registros dentro do request body
   async create(req, res) {
+
+    //criando a schema com o Yup (para comparar o body que estou enviando com o que esta no banco de dados)
+    const schema = Yup.object().shape(
+      {
+        name: Yup.string().required(),
+        email: Yup.string().email().required(),
+        status: Yup.string().uppercase(),
+      }
+    )
+
+    //agora verifico que o schema que montei e valido com o req.body. Se nao for, tomar providencias
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({error: "error on validate schema"})
+    }
+
+
     const customer = await Customer.create(req.body)
     return res.status(201).json(customer)
   }
 
   //atualiza um customer
-  update(req, res) {
-    const id = Number(req.params.id);
-    const {name, site} = req.body;
+  // update(req, res) {
+  //   const id = Number(req.params.id);
+  //   const {name, site} = req.body;
 
-    const index = customers.findIndex(item => item.id === id);
-    const status = index >=0 ? 200 : 404;
+  //   const index = customers.findIndex(item => item.id === id);
+  //   const status = index >=0 ? 200 : 404;
 
-    if(index >=0){
-      customers[index] = {id: Number(id), name, site};
+  //   if(index >=0){
+  //     customers[index] = {id: Number(id), name, site};
+  //   }
+
+  //   return res.status(status).json(customers[index])
+  // }
+
+  //os campos name, email e status ficam como opcionais
+  async update(req, res) {
+    console.log('body retornado:', req.body)
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      status: Yup.string().uppercase(),
+    });
+
+    console.log(schema)
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({error: "Error on validate schema"})
     }
 
-    return res.status(status).json(customers[index])
+    const customer = await Customer.findByPk(req.params.id)
+
+    //antes de rodar a atualizacao, verificamos se o customer existe
+    if (!customer) {
+      return res.status(404).json();
+    }
+
+    await customer.update(req.body)
+
+    return res.json(customer)
   }
 
   //exclui um customer
-  delete(req, res) {
-  //receber o id
-  const id = req.params.id;
+  // delete(req, res) {
+  // //receber o id
+  // const id = req.params.id;
 
-  //localizar o index
-  const index = customers.findIndex(item => item.id === id);
-  const status = index >= 0 ? 200 : 404;
+  // //localizar o index
+  // const index = customers.findIndex(item => item.id === id);
+  // const status = index >= 0 ? 200 : 404;
 
-  //se o index for maior do que zero, significa que achei o customer que procurava
-  if (index >= 0) {
-    //o splice remove o objeto na posicao atual (primeiro parametro) e define a quantidade que sera removida(segundo parametro)
-    costumers.splice(index, 1)
+  // //se o index for maior do que zero, significa que achei o customer que procurava
+  // if (index >= 0) {
+  //   //o splice remove o objeto na posicao atual (primeiro parametro) e define a quantidade que sera removida(segundo parametro)
+  //   costumers.splice(index, 1)
+  // }
+
+  // //no caso do delete, a gente passa o json vazio porque o customer deletado nao precisa ser retornado
+  // return res.status(status).json();
+  // }
+
+  async delete(req, res) {
+  
+    const customer = await Customer.findByPk(req.params.id);
+    
+    if (!customer) {
+      return res.status(404).json();
+    }
+
+    await customer.destroy();
+
+    return res.json()
   }
 
-  //no caso do delete, a gente passa o json vazio porque o customer deletado nao precisa ser retornado
-  return res.status(status).json();
-  }
+
 }
 
 export default new CustomerController();
